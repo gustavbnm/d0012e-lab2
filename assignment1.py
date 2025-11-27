f550125f46e4d9f8c87b4adcca8c97b621f20632
jkgs1 <john@jkgs.se>
2025-11-27 13:41:31 +0100

    Implement a worst-case Θ(n^3)-time recursive algorithm

--- assignment1.py
+++ assignment1.py
@@ -5,25 +5,31 @@ def main():
     n=10
     min_val, max_val = 0, 100
     a = array("i", random.sample(range(min_val,max_val),n))
-    test = worst_case_three(a)
+    test = worst_case_three(a,0,1,2)
 
-def worst_case_three(arr: array):
-    count = 0
+def worst_case_three(arr: array, i, j, k):
     length = len(arr)
 
+    # If i hits the end there is no solution
+    if i >= length:
+        print("Nah")
+        return False
 
-def notRecursive(arr: array):
-    for i in range(len(arr)):
-        x = arr[i]
-        for j in range(i+1, len(arr)):
-            y = arr[j]
-            for k in range(len(arr)):
-                if k!=i and k!= j:
-                    z = arr[k]
-                    if x + y == z:
-                        print(x, y, z)
-                        return True
-    print("nah")
-    return False
+    # If j hits the end we move to the next i
+    if j >= length:
+        return worst_case_three(arr, i+1, i+2, i+3)
+
+    # If k hits the end we move to the next j
+    if k >= length:
+        return worst_case_three(arr, i, j+1, j+2)
+
+    if i != j and i != k and j != k:
+        if arr[i] + arr[j] == arr[k]:
+            print(arr[i], "+", arr[j], "=", arr[k])
+            return True
+
+    # If none of the if statements is matched,
+    # continue the recursion with the next k
+    return worst_case_three(arr, i, j, k+1)
 
 main()
